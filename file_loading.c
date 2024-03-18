0e4593305b1d5eaf749cd90985dbc55a601ac178
nesterova-p <nesterop@DESKTOP-OA2OQN4>
2024-03-18 14:48:11 +0100

    Moduł 1. Sprawdzanie poprawności labiryntu i poprawianie błędów

--- file_loading.c
+++ file_loading.c
@@ -4,15 +4,18 @@
 #include <unistd.h> //dla getopta
 #include "file_loading.h"
 
-// f-cja do wyœwietlania info o u¿yciu programy
+// f-cja do wyszwietlania info o uzyciu programy
 void print_usage(){
     printf("Usage of the programme - program_name -f [maze_file.txt] [-o output_file.txt] [-h]\n");
 
 }
 
-// przetwarzanie argumentów za pomoc¹ getopta
+// przetwarzanie argumentów za pomoca getopta
 void process_input(int argc, char *argv[], char **input_filename, char **output_filename) {
     int opt;
+
+    // Inicjalizacja output_filename na NULL
+    *output_filename = NULL;
 
     while ((opt = getopt(argc, argv, "f:o:h")) != -1) {
         switch (opt) {
@@ -33,42 +36,26 @@ void process_input(int argc, char *argv[], char **input_filename, char **output_
 
     if (*input_filename == NULL){
         print_usage();
-        exit(2); // B³¹d 2. Brak mo¿liwoœci wczytania pliku
+        exit(2); // blad 2. Brak mozliwosci wczytania pliku
     }
 
     if (!file_exists(*input_filename)) {
         fprintf(stderr, "Error. Unable to open input file - %s\n", *input_filename);
-        exit(2); // B³¹d 2. Brak mo¿liwoœci wczytania pliku
+        exit(2); // blad 2. Brak mozliwosci wczytania pliku
     }
 
     if (!is_valid_input_file(*input_filename)) {
         fprintf(stderr, "Error. Invalid input file format - %s\n", *input_filename);
-        exit(4); // B³¹d 4. Nieprawid³owy format pliku
+        exit(4); // blad 4. Nieprawidlowy format pliku
     }
 
     if (!is_valid_maze_format(*input_filename)) {
         fprintf(stderr, "Error. Invalid maze format - %s\n", *input_filename);
-        exit(5); // B³¹d 5. Nieprawid³owy format labiryntu
-}
-}
-
-//pobieranie nazwy pliku z argumetów wiersza poleceñ
-char* get_input_filename(int argc, char *argv[]){
-    char *input_filename = NULL;
-    char *output_filename = NULL;
-    process_input(argc, argv, &input_filename, &output_filename);
-    return input_filename;
+        exit(5); // blad 5. Nieprawidlowy format labiryntu
+    }
 }
 
-//pobierania nazwy pliku wyjœciowego z argumentów wiersza poleceñ
-char* get_output_filename(int argc, char *argv[]) {
-    char *input_filename = NULL;
-    char *output_filename = NULL;
-    process_input(argc, argv, &input_filename, &output_filename);
-    return output_filename;
-}
-
-//sprawdzenie czy plik istnieje w systemie
+// czy plik istnieje w systemie
 bool file_exists(const char *filename){
     FILE *file = fopen(filename, "r");
     if(file){
@@ -78,7 +65,7 @@ bool file_exists(const char *filename){
     return false;
 }
 
-//sprawdzenie czy plik ma prawid³owe rozszerzenie '.txt' oraz czy mo¿na go otworzyæ i wczytaæ
+//sprawdzenie czy plik ma prawidlowe rozszerzenie '.txt' oraz czy mozna go otworzyc i wczytac
 bool is_valid_input_file( const char *filename){
     if (file_exists(filename)){
         const char *extension = ".txt";
@@ -89,49 +76,68 @@ bool is_valid_input_file( const char *filename){
     return false;
 }
 
-//plik wejœciowy jest nieprawidlowy lub nie mo¿na go otworzyæ
-void handle_invalid_input_file(const char *filename){
-    fprintf(stderr, "Error. Invalid input file - %s\n", filename);
-    exit(EXIT_FAILURE);
-}
+//funkcja do testowania poprawnosci formatu labiryntu
+#define MAX_LINE_LENGTH 10000
 
-
-//funkcja do testowania poprawnoœci formatu labiryntu
-bool is_valid_maze_format(const char *filename){
+bool is_valid_maze_format(const char *filename) {
     FILE *file = fopen(filename, "r");
-    if (!file) //je¿eli plik = NULL
-    {
+    if (!file) {
         fprintf(stderr, "Error. Unable to open file - %s\n", filename);
-        exit(EXIT_FAILURE);
+        return false;
     }
 
-    char line[1024];
-    int num_p = 0;
-    int num_k = 0;
-    //num_p oraz num_k to liczniki wejœæ i wyjœæ
+    char line[MAX_LINE_LENGTH];
+    int num_p = 0, num_k = 0;
+    size_t expected_line_length = 0;
+    size_t line_num = 0;
 
-    while(fgets(line, sizeof(line), file)) 
-    {
-        for (int i = 0; line[i]; i++){
-            if (line[i] == 'P')
-            {
-                num_p++;
-            }
-            else if(line[i] == 'K')
-            {
-                num_k++;
-            }
-            else if(line[i] != ' ' && line[i] != 'X') //nieprawid³owy znak
-            {
+    while (fgets(line, sizeof(line), file)) {
+        line_num++;
+
+        // sprawdza dlugosc linii
+        size_t line_length = strlen(line);
+        if (line_length > MAX_LINE_LENGTH) {
+            fprintf(stderr, "Error. Line too long in maze file (line %zu).\n", line_num);
+            fclose(file);
+            return false;
+        }
+
+        // usuwa znak nowej liniji jesli istnieje
+        if (line[line_length - 1] == '\n') {
+            line[line_length - 1] = '\0';
+            line_length--;
+        }
+
+        // czy wszystkie linije maja ta sama dlugosc
+        if (line_num == 1) {
+            expected_line_length = line_length;
+        } else {
+            if (line_length != expected_line_length) {
+                fprintf(stderr, "Error. Inconsistent line length in maze file.\n");
                 fclose(file);
-                return false; 
+                return false;
+            }
+        }
+
+        // czy linija zawiera prawidlowe znaki
+        for (size_t i = 0; i < line_length; i++) {
+            if (line[i] == 'P') {
+                num_p++;
+            } else if (line[i] == 'K') {
+                num_k++;
+            } else if (line[i] != ' ' && line[i] != 'X') {
+                fprintf(stderr, "Error. Invalid character in maze file (line %zu).\n", line_num);
+                fclose(file);
+                return false;
             }
         }
     }
 
     fclose(file);
 
-    if(num_p != 1 || num_k != 1){
+    // czy istnieje tylko jedno wejscie i wyjscie
+    if (num_p != 1 || num_k != 1) {
+        fprintf(stderr, "Error. Incorrect number of entrances or exits in maze file.\n");
         return false;
     }
 
---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "file_loading.h"
+
+int main(int argc, char *argv[]) {
+    // deklaracja zmiennych na nazwy plików
+    char *input_filename = NULL;
+    char *output_filename = NULL;
+
+    // getopt()
+    process_input(argc, argv, &input_filename, &output_filename);
+
+    printf("Input filename: %s\n", input_filename);
+    printf("Output filename: %s\n", output_filename);
+
+    // sprawdzenie czy plik istnieje
+    if (file_exists(input_filename)) {
+        printf("Input file exists.\n");
+    } else {
+        printf("Input file does not exist.\n");
+    }
+
+    // poprawnosc formatu labiryntu
+    if (is_valid_maze_format(input_filename)) {
+        printf("The maze format is valid.\n");
+    } else {
+        printf("The maze format is invalid.\n");
+    }
+
+    // zwolnienie pamiêci zaalokowanej na nazwy plików
+    //free(input_filename);
+    free(output_filename);
+
+    return EXIT_SUCCESS;
+}
