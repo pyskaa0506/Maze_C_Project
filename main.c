99c27845c7215a204225ef67623a2572a38b8992
pyskaa0506 <62877223+pyskaa0506@users.noreply.github.com>
2024-04-06 00:05:22 +0200

    Simplyfied saving chunks

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main(int argc, char *argv[]) {
     char *filepath = "../default_maps/25x50_20.txt";
     int16_t col = 25;
     int16_t row = 50; //remember it is 2 * row + 1 in file
-    int16_t chunk_row_size = 50; // How many rows should be packed into a chunk (file)
+    int16_t chunk_row_size = 40; // How many rows should be packed into a chunk (file)
     txt_file_to_bin_chunks(filepath, col, row, chunk_row_size);
     return 0;
 }
