70fd5af1df18576f863627d6056a607f4be78394
pyskaa0506 <62877223+pyskaa0506@users.noreply.github.com>
2024-04-11 22:10:01 +0200

    Reading bin file

---
+++ bin_chunks_handling.h
@@ -0,0 +1,12 @@
+//
+// Created by pyska on 11.04.2024.
+//
+
+#ifndef MAZE_BIN_BIN_CHUNKS_HANDLING_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+void read_bin_file(char *filepath, int chunk_row_counter);
+#define MAZE_BIN_BIN_CHUNKS_HANDLING_H
+
+#endif //MAZE_BIN_BIN_CHUNKS_HANDLING_H
